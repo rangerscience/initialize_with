6759dd8da859303b632371c6e3f2fc6de773c218
Nicholas Barone <nicktbarone@gmail.com>
2024-08-01 22:26:19 -0700

    bundle gem initialize_with --test=rspec

---
+++ lib/initialize_with.rb
@@ -0,0 +1,6 @@
+require "initialize_with/version"
+
+module InitializeWith
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/initialize_with/version.rb
@@ -0,0 +1,3 @@
+module InitializeWith
+  VERSION = "0.1.0"
+end
